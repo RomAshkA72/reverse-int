8edf5a998d16ac7095df8205ad3db131b9a562fd
Dervoed Roman <dervoedroman05@gmail.com>
2023-08-18 17:06:52 +0300

    feat: result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    const absNum = Math.abs(n);
+    const reversNumber = parseFloat(absNum.toString().split('').reverse().join(''));
+    return  reversNumber;
 }
